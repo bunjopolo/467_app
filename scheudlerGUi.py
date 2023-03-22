46fd64f71c468e49e27d4205673dc9a9ccca8bd8
Spencer Marchand <spenny98m@gmail.com>
2023-03-22 12:45:53 -0700

    modified:   scheudlerGUi.py 	modified:   scheudlerGUi.py

--- scheudlerGUi.py
+++ scheudlerGUi.py
@@ -9,52 +9,106 @@ import numpy as np
 
 class Ui_MainWindow(QMainWindow):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(830, 691)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.graph_label = QtWidgets.QLabel(self.centralwidget)
-        self.graph_label.setGeometry(QtCore.QRect(20, 10, 791, 391))
-        self.graph_label.setText("")
-        self.graph_label.setObjectName("graph_label")
-        self.phase_label = QtWidgets.QLabel(self.centralwidget)
-        self.phase_label.setGeometry(QtCore.QRect(60, 470, 41, 16))
-        self.phase_label.setObjectName("phase_label")
-        self.period_label = QtWidgets.QLabel(self.centralwidget)
-        self.period_label.setGeometry(QtCore.QRect(60, 500, 41, 16))
-        self.period_label.setObjectName("period_label")
-        self.execution_label = QtWidgets.QLabel(self.centralwidget)
-        self.execution_label.setGeometry(QtCore.QRect(10, 530, 101, 20))
-        self.execution_label.setObjectName("execution_label")
-        self.phas_edit = QtWidgets.QLineEdit(self.centralwidget)
-        self.phas_edit.setGeometry(QtCore.QRect(120, 470, 581, 21))
-        self.phas_edit.setObjectName("phas_edit")
-        self.period_edit = QtWidgets.QLineEdit(self.centralwidget)
-        self.period_edit.setGeometry(QtCore.QRect(120, 500, 581, 21))
-        self.period_edit.setObjectName("period_edit")
-        self.execution_edit = QtWidgets.QLineEdit(self.centralwidget)
-        self.execution_edit.setGeometry(QtCore.QRect(120, 530, 581, 21))
-        self.execution_edit.setObjectName("execution_edit")
-        self.deadline_label = QtWidgets.QLabel(self.centralwidget)
-        self.deadline_label.setGeometry(QtCore.QRect(50, 560, 60, 16))
-        self.deadline_label.setObjectName("deadline_label")
-        self.deadline_edit = QtWidgets.QLineEdit(self.centralwidget)
-        self.deadline_edit.setGeometry(QtCore.QRect(120, 560, 581, 21))
-        self.deadline_edit.setObjectName("deadline_edit")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(270, 590, 281, 51))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton.clicked.connect(lambda : self.schedule())
-        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
-        self.comboBox.setGeometry(QtCore.QRect(290, 430, 241, 26))
-        self.comboBox.setEditable(True)
-        self.comboBox.setObjectName("comboBox")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
+    
+        screen = QApplication.primaryScreen() #get the screen size
+        global size #make it global so we can use it in other functions
+        size = screen.size() #get the size of the screen
+        MainWindow.setObjectName("MainWindow") #set the name of the window
+        MainWindow.resize(830, size.height()) #set the size of the window
+        MainWindow.setFixedWidth(830) #set the width of the window
+        self.centralwidget = QtWidgets.QWidget(MainWindow) #set the central widget
+        self.centralwidget.setObjectName("centralwidget") #set the name of the central widget
+
+
+        self.graph_label = QtWidgets.QLabel(self.centralwidget) #set the graph label
+        self.graph_label.setGeometry(QtCore.QRect(20, 10, 791, 391)) #set the size and location of the graph label
+        self.graph_label.setText("") #set the text of the graph label
+        self.graph_label.setObjectName("graph_label") #set the name of the graph label
+        self.graph_label.setStyleSheet("border: 1px solid black;") #set the border of the graph label
+
+        self.phase_label = QtWidgets.QLabel(self.centralwidget) #set the phase label
+        self.phase_label.setGeometry(QtCore.QRect(25, size.height()-350, 101, 16)) #set the size and location of the phase label
+        self.phase_label.setObjectName("phase_label") #set the name of the phase label
+
+        self.period_label = QtWidgets.QLabel(self.centralwidget) #set the period label
+        self.period_label.setGeometry(QtCore.QRect(60, size.height()-320, 41, 16)) #set the size and location of the period label
+        self.period_label.setObjectName("period_label") #set the name of the period label
+         
+        self.execution_label = QtWidgets.QLabel(self.centralwidget) #set the execution label
+        self.execution_label.setGeometry(QtCore.QRect(10, size.height()-290, 101, 20)) #set the size and location of the execution label
+        self.execution_label.setObjectName("execution_label") #set the name of the execution label
+
+        self.runTime_label = QtWidgets.QLabel(self.centralwidget) #set the
+        self.runTime_label.setGeometry(QtCore.QRect(50, size.height()-380, 60, 16)) #set the size and location of the run time label
+        self.runTime_label.setObjectName("runTime_label") #set the name of the run time label
+        #Done and in right position
+        # self.time_label = QtWidgets.QLabel(self.centralwidget)
+        # self.time_label.setGeometry(QtCore.QRect(10, size.height()-230, 101, 20))
+        # self.time_label.setObjectName("time_label")
         
+
+        self.phas_edit = QtWidgets.QLineEdit(self.centralwidget) #set the phase edit
+        self.phas_edit.setGeometry(QtCore.QRect(120, size.height()-350, 581, 21)) #set the size and location of the phase edit
+        self.phas_edit.setObjectName("phas_edit") #set the name of the phase edit
+ 
+        self.period_edit = QtWidgets.QLineEdit(self.centralwidget) #set the period edit
+        self.period_edit.setGeometry(QtCore.QRect(120, size.height()-320, 581, 21)) #set the size and location of the period edit
+        self.period_edit.setObjectName("period_edit") #set the name of the period edit
+
+        self.execution_edit = QtWidgets.QLineEdit(self.centralwidget) #set the execution edit
+        self.execution_edit.setGeometry(QtCore.QRect(120, size.height()-290, 581, 21)) #set the size and location of the execution edit
+        self.execution_edit.setObjectName("execution_edit") #set the name of the execution edit
+
+        #done and in right position
+        self.deadline_label = QtWidgets.QLabel(self.centralwidget) #set the deadline label
+        self.deadline_label.setGeometry(QtCore.QRect(50, size.height()-260, 60, 16)) #set the size and location of the deadline label
+        self.deadline_label.setObjectName("deadline_label") #set the name of the deadline label
+
+        #done and in right position
+        self.deadline_edit = QtWidgets.QLineEdit(self.centralwidget) #set the deadline edit
+        self.deadline_edit.setGeometry(QtCore.QRect(120, size.height()-260, 581, 21)) #set the size and location of the deadline edit
+        self.deadline_edit.setObjectName("deadline_edit") #set the name of the deadline edit
+
+        self.runTime_edit = QtWidgets.QLineEdit(self.centralwidget) #set the run time edit
+        self.runTime_edit.setGeometry(QtCore.QRect(120, size.height()-380, 581, 21)) #set the size and location of the run time edit
+        self.runTime_edit.setObjectName("runTime_edit") #set the name of the run time edit
+        self.runTime_edit.setValidator(QtGui.QIntValidator()) #set the validator of the run time edit to only accept integers
+        #done and in right position
+        # self.time_quant = QtWidgets.QLineEdit(self.centralwidget)
+        # self.time_quant.setGeometry(QtCore.QRect(120, size.height()-230, 581, 21))
+        # self.time_quant.setObjectName("time_quant")
+
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget) #set the push button
+        self.pushButton.setGeometry(QtCore.QRect(270, size.height()-200, 281, 51)) #set the size and location of the push button
+        self.pushButton.setObjectName("pushButton") #set the name of the push button
+        self.pushButton.clicked.connect(lambda : self.schedule()) #connect the push button to the schedule function
+
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget) #set the combo box
+        self.comboBox.setGeometry(QtCore.QRect(290, size.height()-420, 241, 26)) #set the size and location of the combo box
+        self.comboBox.setEditable(False) #set the combo box to editable
+        self.comboBox.setObjectName("comboBox") #set the name of the combo box
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.currentIndexChanged.connect(lambda : self.updateUI(MainWindow))
+
+        self.time_quant = QtWidgets.QLineEdit(self.centralwidget)
+        self.time_quant.setGeometry(QtCore.QRect(120, size.height()-230, 581, 21))
+        self.time_quant.setObjectName("time_quant")
+       
+        #hide the time quantum
+        self.time_quant.hide()
+        
+        self.quant_label = QtWidgets.QLabel(self.centralwidget)
+        self.quant_label.setGeometry(QtCore.QRect(10, size.height()-230, 101, 20))
+        self.quant_label.setObjectName("time_label")
+        self.quant_label.setText("Time Quantum")
+        self.quant_label.hide()
+
+
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 830, 24))
@@ -66,66 +120,207 @@ class Ui_MainWindow(QMainWindow):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.updateUI(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.phase_label.setText(_translate("MainWindow", "Phase "))
+        self.phase_label.setText(_translate("MainWindow", "Arrival Time"))
         self.period_label.setText(_translate("MainWindow", "Period"))
         self.execution_label.setText(_translate("MainWindow", "Execution TIme"))
         self.deadline_label.setText(_translate("MainWindow", "Deadline"))
+        self.runTime_label.setText(_translate("MainWindow", "Run Time"))
+        
+        # self.time_label.setText(_translate("MainWindow", "Time Quantum"))
         self.pushButton.setText(_translate("MainWindow", "Schedule! "))
         self.comboBox.setItemText(0, _translate("MainWindow", "Pick an Algorithm"))
         self.comboBox.setItemText(1, _translate("MainWindow", "First come first serve"))
         self.comboBox.setItemText(2, _translate("MainWindow", "Round robin"))
         self.comboBox.setItemText(3, _translate("MainWindow", "Rate Monotonic"))
         self.comboBox.setItemText(4, _translate("MainWindow", "Shortest job first"))
+        
         self.comboBox.isEditable()
+
+    def updateUI(self,MainWindow):
+        #update the windwow based on what combo box is selected
+        if self.comboBox.currentIndex() == 0:
+            self.runTime_edit.setReadOnly(True)
+            
+            self.phas_edit.setReadOnly(True)
+            self.period_edit.setReadOnly(True)
+            self.execution_edit.setReadOnly(True)
+            self.deadline_edit.setReadOnly(True)
+            self.time_quant.hide()
+            self.quant_label.hide()
+
+        if self.comboBox.currentIndex() == 1:
+            self.runTime_edit.setReadOnly(False)
+            self.phas_edit.setReadOnly(False)
+            self.period_edit.setReadOnly(False)
+            self.execution_edit.setReadOnly(False)
+            self.deadline_edit.setReadOnly(False)
+            self.time_quant.hide()
+            self.quant_label.hide()
+
+        if self.comboBox.currentIndex() == 2:
+            self.runTime_edit.setReadOnly(False)
+            self.phas_edit.setReadOnly(False)
+            self.period_edit.setReadOnly(False)
+            self.execution_edit.setReadOnly(False)
+            self.deadline_edit.setReadOnly(False)
+            self.time_quant.show()
+            self.quant_label.show()
+
+        if self.comboBox.currentIndex() == 3:
+            self.runTime_edit.setReadOnly(False)
+            self.phas_edit.setReadOnly(False)
+            self.period_edit.setReadOnly(False)
+            self.execution_edit.setReadOnly(False)
+            self.deadline_edit.setReadOnly(False)
+            self.time_quant.hide()
+            self.quant_label.hide()
+            
+        if self.comboBox.currentIndex() == 4:
+            self.runTime_edit.setReadOnly(False)
+            self.phas_edit.setReadOnly(False)
+            self.period_edit.setReadOnly(False)
+            self.execution_edit.setReadOnly(False)
+            self.deadline_edit.setReadOnly(False)
+            self.time_quant.hide()
+            self.quant_label.hide()
+
+
+
 
     def schedule(MainWindow):
 
-        test = "When you push the button it shcedules the tasks"
-        MainWindow.graph_label.setText(test)
         number = MainWindow.comboBox.currentIndex()
-
-
-        phase = MainWindow.phas_edit.text()
-        phase_arr = []
+        #grab the string from the phase box and create an array for phase
+        arrival = MainWindow.phas_edit.text()
+        arrival_arr = []
+        #grab the string from the period box and create an array for period
         period = MainWindow.period_edit.text()
         period_arr = []
+        #grab the string from the execution box and create an array for execution
         execution = MainWindow.execution_edit.text()
         execution_arr = []
+        #grab the string from the deadline box and create an array for deadline
         deadline = MainWindow.deadline_edit.text()
         deadline_arr = []
+        #grab the string from the time quantum box and create an array for time quantum
+        
+        time_quant = MainWindow.time_quant.text()
+        time_quant_arr = []
 
-        for i in phase:
+        #Grab the string from the runtime box and greate a integer variable for it
+        
+        run_time = MainWindow.runTime_edit.text()
+        if not run_time:
+            MainWindow.graph_label.setText("Please enter a run time")
+            return
+        else:
+            run_time = int(run_time)
+
+        #check if the user entered numbers only
+        for i in arrival:
             if i != " " and  not i.isdigit():
-                print("Please enter numbers only")
+                MainWindow.graph_label.setText("Please enter numbers only")
                 return
         for i in period:
             if i != " " and  not i.isdigit():
-                print("Please enter numbers only")
+                MainWindow.graph_label.setText("Please enter numbers only")
                 return
         for i in execution:
             if i != " " and  not i.isdigit():
-                print("Please enter numbers only")
+                MainWindow.graph_label.setText("Please enter numbers only")                   
                 return
         for i in deadline:
             if i != " " and  not i.isdigit():
-                print("Please enter numbers only")
+                MainWindow.graph_label.setText("Please enter numbers only")              
                 return
         
+            for i in time_quant:
+                if number ==2 and i != " " and  not i.isdigit():
+                    MainWindow.graph_label.setText("Please enter numbers only")              
+                    return
+    
 
-        phase_arr = [int(i) for i in phase.split(" ")]
-        period_arr = [int(i) for i in period.split(" ")]
-        execution_arr = [int(i) for i in execution.split(" ")]
-        deadline_arr = [int(i) for i in deadline.split(" ")]
-
-        if len(phase_arr) != len(period_arr) or len(period_arr) != len(execution_arr) or len(execution_arr) != len(deadline_arr):
+        #check if the user entered the same number of tasks for each field
+        if len(arrival_arr) != len(period_arr) or len(period_arr) != len(execution_arr) or len(execution_arr) != len(deadline_arr):
             MainWindow.graph_label.setText("Please enter the same number of tasks for each field")
             return
-        
 
+        #Check if any of the fields are empty and if they are then populate it with 0's of the size of one of the non empty fields
+        if not arrival:
+            if len(period_arr) != 0:
+                arrival_arr = [0]*len(period_arr)
+            elif len(execution_arr) != 0:
+                arrival_arr = [0]*len(execution_arr)
+            elif len(deadline_arr) != 0:
+                arrival_arr = [0]*len(deadline_arr)
+            else:
+                MainWindow.graph_label.setText("Please enter at least one task")
+        else: 
+            arrival_arr = [int(i) for i in arrival.split(" ")]
+                
+        
+        if not period:
+            if len(arrival_arr) != 0:
+                period_arr = [0]*len(arrival_arr)
+            elif len(execution_arr) != 0:
+                period_arr = [0]*len(execution_arr)
+            elif len(deadline_arr) != 0:
+                period_arr = [0]*len(deadline_arr)
+            else:
+                MainWindow.graph_label.setText("Please enter at least one task")
+        else:
+            period_arr = [int(i) for i in period.split(" ")]
+                
+        
+        if not execution:
+            if len(arrival_arr) != 0:
+                execution_arr = [0]*len(arrival_arr)
+            elif len(period_arr) != 0:
+                execution_arr = [0]*len(period_arr)
+            elif len(deadline_arr) != 0:
+                execution_arr = [0]*len(deadline_arr)
+            else:
+                MainWindow.graph_label.setText("Please enter at least one task")
+        else:
+            execution_arr = [int(i) for i in execution.split(" ")]
+                
+        
+        if not deadline:
+            if len(arrival_arr) != 0:
+                deadline_arr = [0]*len(arrival_arr)
+            elif len(period_arr) != 0:
+                deadline_arr = [0]*len(period_arr)
+            elif len(execution_arr) != 0:
+                deadline_arr = [0]*len(execution_arr)
+            else:
+                MainWindow.graph_label.setText("Please enter at least one task")
+        else:
+            deadline_arr = [int(i) for i in deadline.split(" ")]
+        
+        if not time_quant:
+            if len(arrival_arr) != 0:
+                time_quant_arr = [0]*len(arrival_arr)
+            elif len(period_arr) != 0:
+                time_quant_arr = [0]*len(period_arr)
+            elif len(execution_arr) != 0:
+                time_quant_arr = [0]*len(execution_arr)
+            elif len(deadline_arr) != 0:
+                time_quant_arr = [0]*len(deadline_arr)
+            else:
+                MainWindow.graph_label.setText("Please enter at least one task")
+        else:
+            time_quant_arr = [int(i) for i in time_quant.split(" ")]
+
+
+        #Create a dictionary to hold all the arrays
+        processes = {'Run Time': run_time, 'arrival': arrival_arr, 'period': period_arr, 'execution': execution_arr, 'deadline': deadline_arr, 'time_quantum': time_quant_arr}
+        print(processes)
+        #Get the number from the combobox and run the corresponding algorithm
         if number ==0:
             MainWindow.graph_label.setText("Please pick an algorithm in the drop down list")
             return
